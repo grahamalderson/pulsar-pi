4ce2e71874af4324a8dba6d5baaf0b46eaf861db
Graham Alderson <grahamalderson@users.noreply.github.com>
2020-12-29 21:50:39 -0700

    Update calibre-web.sh

--- apps/calibre-web.sh
+++ apps/calibre-web.sh
@@ -2,8 +2,8 @@
 docker run --detach --restart unless-stopped \
 --name calibre-web \
 --publish 8083:8083 \
---volume /media/pi/usb-drive/apps/calibre-web/config:/config \
---volume /media/pi/usb-drive/media/books:/books \
+--volume /media/pi/fast/apps/calibre-web/config:/config \
+--volume /media/pi/files/media/books:/books \
 -e PUID=1000 \
 -e PGID=1000 \
 -e DOCKER_MODS=linuxserver/calibre-web:calibre \
