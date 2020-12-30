5507b521a2640d1660cb9962bf314cb568f7ee32
Graham Alderson <grahamalderson@users.noreply.github.com>
2020-12-29 21:53:03 -0700

    Update nginx.sh

--- apps/nginx.sh
+++ apps/nginx.sh
@@ -2,5 +2,6 @@
 docker run --detach --restart unless-stopped \
 --name nginx \
 --publish 80:80 \
---volume /media/pi/usb-drive/apps/website:/usr/share/nginx/html \
+--volume /media/pi/fast/apps/pulsar-site:/usr/share/nginx/html \
+--volume /media/pi/files/offline:/usr/share/nginx/html/offline \
 nginx
