2eaf886af3e604d205b1503980f7d00966e8320d
Graham Alderson <grahamalderson@users.noreply.github.com>
2020-12-29 21:54:51 -0700

    Update samba.sh

--- apps/samba.sh
+++ apps/samba.sh
@@ -4,6 +4,8 @@ docker run --detach --restart unless-stopped \
 --name samba \
 --publish 139:139 \
 --publish 445:445 \
---volume /media/pi:/share \
-dperson/samba \
--s "share;/share;yes;no;yes;;;;"
+--volume /media/pi/files/share:/shares/public \
+--volume /media/pi/files:/shares/readonly \
+dperson/samba
+-s "public;/shares/public;yes;no;yes;;;;" \
+-s "readonly;/shares/readonly;yes;yes;yes;;;;"
