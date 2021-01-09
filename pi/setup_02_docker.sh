f1c4a934aea3cd18b65b194be52fa392231bd1fe
Graham Alderson <grahamalderson@users.noreply.github.com>
2021-01-08 17:47:08 -0700

    Fix startup order
    Extra lines to add a systemd .d conf file for the docker service to have it start after the usb drive mounting service starts.

--- pi/setup_02_docker.sh
+++ pi/setup_02_docker.sh
@@ -3,4 +3,10 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker pi
 sudo systemctl enable docker
+sudo mkdir /etc/systemd/system/docker.service.d
+sudo bash -c 'cat > /etc/systemd/system/docker.service.d/override.conf << "END"
+[UNIT]
+After=udisks2.service
+END
+'
 sudo reboot
