7f5ca51cc1a27c5852e69f36a7e3eca5d930f0a3
Graham Alderson <grahamalderson@users.noreply.github.com>
2021-01-13 16:52:03 -0700

    Update murmur.sh

--- apps/murmur.sh
+++ apps/murmur.sh
@@ -3,6 +3,6 @@ docker run --detach --restart unless-stopped \
 --name murmur \
 --publish 64738:64738 \
 --publish 64738:64738/udp \
---volume /media/pi/usb-drive/apps/mumble/conf:/opt/mumble/conf \
---volume /media/pi/usb-drive/apps/mumble/data:/opt/mumble/data \
+--volume /media/pi/usb-drive/apps/murmur/conf:/opt/mumble/conf \
+--volume /media/pi/usb-drive/apps/murmur/data:/opt/mumble/data \
 magicalex/mumble:latest
