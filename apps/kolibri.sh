14184c0e04a4beee48fb23fed78cd74357a707ae
Graham Alderson <grahamalderson@users.noreply.github.com>
2021-01-30 11:09:38 -0700

    Update kolibri.sh

--- apps/kolibri.sh
+++ apps/kolibri.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build https://github.com/grahamalderson/kolibri.git#patch-1:docker -t local/kolibri -f arm-Dockerfile
+docker build https://github.com/grahamalderson/kolibri.git:docker -t local/kolibri -f arm-Dockerfile
 docker run --detach --restart unless-stopped \
 --name kolibri \
 --publish 8080:8080 \
