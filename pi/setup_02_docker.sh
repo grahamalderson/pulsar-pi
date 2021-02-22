ae46b4fb32eef3b6d80e1c8ed751fd8bb98cfcc9
Graham Alderson <grahamalderson@users.noreply.github.com>
2021-02-21 17:30:26 -0700

    docker script update

--- pi/setup_02_docker.sh
+++ pi/setup_02_docker.sh
@@ -6,8 +6,7 @@ sudo systemctl enable docker
 sudo mkdir /etc/systemd/system/docker.service.d
 sudo bash -c 'cat > /etc/systemd/system/docker.service.d/override.conf << "END"
 [UNIT]
-After=udisks2.service
-After=media-pi-files.mount
+After=mnt-files.mount
 END
 '
 sudo reboot
