b20063a3a0e64f27908f00d5f087c693dd883a37
Graham Alderson <grahamalderson@users.noreply.github.com>
2021-03-15 11:15:10 -0600

    Update calibre-web.sh

--- apps/calibre-web.sh
+++ apps/calibre-web.sh
@@ -3,7 +3,7 @@ docker run --detach --restart unless-stopped \
 --name calibre-web \
 --publish 8083:8083 \
 --volume /home/pi/apps/calibre-web/config:/config \
---volume /mnt/files/media/books:/books \
+--volume /home/pi/apps/calibre-web/books:/books \
 -e PUID=1000 \
 -e PGID=1000 \
 -e DOCKER_MODS=linuxserver/calibre-web:calibre \
