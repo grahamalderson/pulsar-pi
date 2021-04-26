92277fda5f7c51947d36ed572245918fa7457086
Graham Alderson <grahamalderson@users.noreply.github.com>
2021-04-26 01:04:32 -0700

    fix samba

--- apps/samba.sh
+++ apps/samba.sh
@@ -4,11 +4,11 @@ docker run --detach --restart unless-stopped \
 --publish 139:139 \
 --publish 445:445 \
 --volume /mnt/files/share:/shares/public \
---volume /mnt/files:/shares/readonly \
+--volume /mnt/files:/shares/files \
 --volume /home/pi/apps/calibre-web/books:/shares/library \
 -e USERID=1000 \
 -e GROUPID=1000 \
 dperson/samba \
 -s "public;/shares/public;yes;no;yes;;;;" \
--s "readonly;/shares/readonly;yes;yes;yes;;;;" \
+-s "readonly;/shares/files;yes;yes;yes;;;;" \
 -s "library;/shares/library;yes;no;yes;;;;"
