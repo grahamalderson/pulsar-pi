c9f1298aee306898c2b859e9be010bcfa73a99db
Graham Alderson <grahamalderson@users.noreply.github.com>
2021-04-26 01:01:10 -0700

    Create modsamba.sh

---
+++ apps/modsamba.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+docker run --detach --restart unless-stopped \
+--name samba \
+--publish 139:139 \
+--publish 445:445 \
+--volume /mnt/files/share:/shares/public \
+--volume /mnt/files:/shares/readonly \
+--volume /mnt/files:/shares/files \
+--volume /home/pi/apps/calibre-web/books:/shares/library \
+-e USERID=1000 \
+-e GROUPID=1000 \
+dperson/samba \
+-s "public;/shares/public;yes;no;yes;;;;" \
+-s "readonly;/shares/readonly;yes;yes;yes;;;;" \
+-s "files;/shares/files;yes;no;yes;;;;" \
+-s "library;/shares/library;yes;no;yes;;;;"
